a3f0f20dceb685c40cc965d8fb90d5e513c52a34
Holamite <olamideadebara@gmail.com>
2024-04-24 22:35:24 +0100

    little change

--- src/components/events/EventTicket/Poap.jsx
+++ src/components/events/EventTicket/Poap.jsx
@@ -12,18 +12,18 @@ const Poap = () => {
             style={{
               clipPath:
                 "polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)",
-              background: "#9A8873",
+              background: "#FDBCB4",
             }}
           >
             <div
               className="h-[280px] w-[280px] flex items-center justify-center flex-col"
               style={{
                 clipPath:
-                  "polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)",
-                background: " #3A2618",
+                  "polygon(50% 0%, 80% 10%, 100% 35%, 100% 70%, 80% 90%, 50% 100%, 20% 90%, 0% 70%, 0% 35%, 20% 10%)",
+                background: " #5A0619",
               }}
             >
-              <div className="flex items-center justify-center flex-col gap-2 text-[#fff]">
+              <div className="flex items-center justify-center flex-col gap-4 text-[#fff]">
                 <div className="flex items-center justify-center gap-2">
                   <TriangleLogo size="48" className="text-[#fff]" />
                   <p className="font-bold">NFTix</p>
@@ -31,9 +31,13 @@ const Poap = () => {
 
                 <h1 className="italic font-light ">Proof of attendance</h1>
 
-                <p className="font-bold font-mono">{title}</p>
+                <p className="text-[1.3rem] text-center font-bold font-mono">
+                  {title}
+                </p>
 
-                <p className="font-thin">1 day course | 7 CPE Credits</p>
+                <p className="font-thin text-[0.6rem]">
+                  1 day course | 7 CPE Credits
+                </p>
               </div>
             </div>
           </div>
